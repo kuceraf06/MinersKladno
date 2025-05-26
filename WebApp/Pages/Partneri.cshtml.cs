8864ed8e1c2778723c94b3ceda14493b93bfa648
Pavel Štilip <stilip@outlook.com>
2025-05-26 13:01:31 +0200

    Nové stránky - scm, partneři, nábory

---
+++ WebApp/Pages/Partneri.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Miners.Web.WebApp.Pages;
+
+public class Partneri : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
---
+++ WebApp/Pages/Scm.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Miners.Web.WebApp.Pages;
+
+public class Scm : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
